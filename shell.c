5c23efcd5846b52b2d0a6ce0d46eb1cc12b3c248
Juan-Mendez <jrjuanj01@gmail.com>
2024-04-24 16:25:32 -0400

    Added all files to the repository

---
+++ shell.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+
+/**
+ * main - acts as an interactive shell
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char *cmnd, *copy, **args, **path;
+	size_t len = 0;
+	int idx, f,  input = 0;
+
+	while (1)
+	{
+		printf("$ ");
+		if (getline(&cmnd, &len, stdin) == -1)
+		{
+			perror("Memory allocation error");
+			exit(EXIT_FAILURE);
+		}
+		input++;
+		cmnd[strcspn(cmnd, "\n")] = '\0';
+		args = token_maker(cmnd);
+		if (args[0] == NULL)
+		{
+			free(args);
+			continue;
+		}
+		if (strcmp(args[0], "exit") == 0)
+			break;
+		if (access(args[0], X_OK) == 0)
+			executer(args);
+		path = pathfinder("PATH", args);
+		if (path == NULL)
+		//executer(args);
+		//else
+			printf("./hsh: %i: %s not found\n", input, args[0]);
+		free(args);
+	}
+	free(cmnd);
+	free(args);
+	return (0);
+}
